654c3b90d3695685be93ab809ff398b23b8ba7fb
Ernest Wambua <ernestwambua2@gmail.com>
2021-07-26 20:13:57 +0300

    added the table view and enabled the search field

--- src/application/controllers/views/UnitsController.java
+++ src/application/controllers/views/UnitsController.java
@@ -3,24 +3,32 @@ package application.controllers.views;
 import application.controllers.UnitController;
 import application.models.Unit;
 import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class UnitsController {
+public class UnitsController implements Initializable {
 
     @FXML
     Button homeButton;
@@ -47,20 +55,81 @@ public class UnitsController {
     TableView<Unit> unitsTable;
 
     @FXML
-    VBox vBox;
+    TableColumn<Unit, String> unitNameCol;
 
-    public void populateTable(ActionEvent event) {
-        ObservableList<Unit> units = FXCollections.observableArrayList(UnitController.getUnits());
+    @FXML
+    TableColumn<Unit, String> unitCodeCol;
 
-        TableColumn<Unit, String> unitNameCol = new TableColumn<>("Unit Name");
-        TableColumn<Unit, String> unitCodeCol = new TableColumn<>("Unit Code");
-        TableColumn<Unit, String> unitLecCol = new TableColumn<>("Lecturer");
-        TableColumn<Unit, Integer> unitPagesCol = new TableColumn<>("Pages");
+    @FXML
+    TableColumn<Unit, String> lecturerCol;
 
-        unitsTable.getColumns().addAll(unitNameCol, unitCodeCol, unitLecCol, unitPagesCol);
+    @FXML
+    TableColumn<Unit, Integer> pagesCol;
 
-        vBox.getChildren().add(unitsTable);
+    @FXML
+    TextField searchTextField;
 
+    @FXML
+    DialogPane dialogPane;
+
+    ObservableList<Unit> unitsObservableList = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        List<Unit> units = UnitController.getUnits();
+
+        assert  units != null;
+        unitsObservableList.addAll(units);
+
+        unitNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        unitCodeCol.setCellValueFactory(new PropertyValueFactory<>("code"));
+        lecturerCol.setCellValueFactory(new PropertyValueFactory<>("lecturer"));
+        pagesCol.setCellValueFactory(new PropertyValueFactory<>("code"));
+
+        unitsTable.setItems(unitsObservableList);
+
+        FilteredList<Unit> filteredUnits = new FilteredList<>(unitsObservableList, b -> true);
+
+        // adding a change listener to the search text field
+        searchTextField.textProperty().addListener((observableValue, oldValue, newValue) -> filteredUnits.setPredicate(unitItems -> {
+
+            // if the search-field is empty then return all the values
+            if (newValue.isEmpty() || newValue.isBlank()) {
+                return true;
+            }
+
+            String searchKeyword = newValue.toLowerCase();
+
+            if(unitItems.getName().toLowerCase().contains(searchKeyword)) {
+                return true;
+            } else if (unitItems.getLecturer().toLowerCase().contains(searchKeyword)) {
+                return true;
+            } else if (unitItems.getCode().contains(searchKeyword)) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }));
+
+        SortedList<Unit> sortedData = new SortedList<>(filteredUnits);
+
+        sortedData.comparatorProperty().bind(unitsTable.comparatorProperty());
+
+        unitsTable.setItems(sortedData);
+    }
+
+    public void addUnit(ActionEvent event) {
+
+    }
+
+    public void deleteUnit(ActionEvent event) {
+
+    }
+
+    public void refreshView(ActionEvent event) throws IOException {
+        setView("Units.fxml", event);
     }
 
     public void changeView(ActionEvent event) throws IOException {
